c62edbe228c58293981c3f88d81bed70e3fd4f79
Michael Rodenhurst <mr@ukpc.net>
2017-09-15 15:48:46 +0100

    Set resolution to 720p for default testing, add hero/attribute enums

---
+++ core/src/com/satgnu/herotrainer/entity/Hero.java
@@ -0,0 +1,21 @@
+package com.satgnu.herotrainer.entity;
+
+public class Hero extends Entity {
+
+    protected BaseAttributes attributes;
+
+    public Hero()
+    {
+        super();
+    }
+
+    public void Update()
+    {
+
+    }
+
+    public void Draw()
+    {
+
+    }
+}
